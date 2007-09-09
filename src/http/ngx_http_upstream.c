reeze/nginx

commit 162b4aab4720cbc0e46487ad8ee966a6a0ebc185

separate ngx_http_upstream_test_connect() and use it in ngx_http_upstream_process_header() path too
git-svn-id: svn://svn.nginx.org/nginx/trunk@1154 c3fe7df1-7212-e011-8a91-001109144009

src/http/ngx_http_upstream.c
@@ -21,6 +21,7 @@ static void ngx_http_upstream_send_request(ngx_http_request_t *r,
     ngx_http_upstream_t *u);
 static void ngx_http_upstream_send_request_handler(ngx_event_t *wev);
 static void ngx_http_upstream_process_header(ngx_event_t *rev);
+static ngx_int_t ngx_http_upstream_test_connect(ngx_connection_t *c);
 static void ngx_http_upstream_process_body_in_memory(ngx_event_t *rev);
 static void ngx_http_upstream_send_response(ngx_http_request_t *r,
     ngx_http_upstream_t *u);
@@ -757,8 +758,7 @@ ngx_http_upstream_reinit(ngx_http_request_t *r, ngx_http_upstream_t *u)
 static void
 ngx_http_upstream_send_request(ngx_http_request_t *r, ngx_http_upstream_t *u)
 {
-    int                rc, err;
-    socklen_t          len;
+    ngx_int_t          rc;
     ngx_connection_t  *c;
 
     c = u->peer.connection;
@@ -766,41 +766,9 @@ ngx_http_upstream_send_request(ngx_http_request_t *r, ngx_http_upstream_t *u)
     ngx_log_debug0(NGX_LOG_DEBUG_HTTP, c->log, 0,
                    "http upstream send request");
 
-    if (!u->request_sent) {
-
-#if (NGX_HAVE_KQUEUE)
-
-        if (ngx_event_flags & NGX_USE_KQUEUE_EVENT)  {
-            if (c->write->pending_eof) {
-                (void) ngx_connection_error(c, c->write->kq_errno,
-                                    "kevent() reported that connect() failed");
-                ngx_http_upstream_next(r, u, NGX_HTTP_UPSTREAM_FT_ERROR);
-                return;
-            }
-
-        } else
-#endif
-        {
-            err = 0;
-            len = sizeof(int);
-
-            /*
-             * BSDs and Linux return 0 and set a pending error in err
-             * Solaris returns -1 and sets errno
-             */
-
-            if (getsockopt(c->fd, SOL_SOCKET, SO_ERROR, (void *) &err, &len)
-                == -1)
-            {
-                err = ngx_errno;
-            }
-
-            if (err) {
-                (void) ngx_connection_error(c, err, "connect() failed");
-                ngx_http_upstream_next(r, u, NGX_HTTP_UPSTREAM_FT_ERROR);
-                return;
-            }
-        }
+    if (!u->request_sent && ngx_http_upstream_test_connect(c) != NGX_OK) {
+	ngx_http_upstream_next(r, u, NGX_HTTP_UPSTREAM_FT_ERROR);
+        return;
     }
 
     c->log->action = "sending request to upstream";
@@ -942,6 +910,11 @@ ngx_http_upstream_process_header(ngx_event_t *rev)
 
     if (rev->timedout) {
         ngx_http_upstream_next(r, u, NGX_HTTP_UPSTREAM_FT_TIMEOUT);
+        return;
+    }
+
+    if (!u->request_sent && ngx_http_upstream_test_connect(c) != NGX_OK) {
+	ngx_http_upstream_next(r, u, NGX_HTTP_UPSTREAM_FT_ERROR);
         return;
     }
 
@@ -1274,6 +1247,48 @@ ngx_http_upstream_process_header(ngx_event_t *rev)
     rev->handler = ngx_http_upstream_process_body_in_memory;
 
     ngx_http_upstream_process_body_in_memory(rev);
+}
+
+
+static ngx_int_t
+ngx_http_upstream_test_connect(ngx_connection_t *c)
+{
+    int        err;
+    socklen_t  len;
+
+#if (NGX_HAVE_KQUEUE)
+
+    if (ngx_event_flags & NGX_USE_KQUEUE_EVENT)  {
+	if (c->write->pending_eof) {
+	    (void) ngx_connection_error(c, c->write->kq_errno,
+			            "kevent() reported that connect() failed");
+	    return NGX_ERROR;
+	}
+
+    } else
+#endif
+    {
+	err = 0;
+	len = sizeof(int);
+
+	/*
+	 * BSDs and Linux return 0 and set a pending error in err
+	 * Solaris returns -1 and sets errno
+	 */
+
+	if (getsockopt(c->fd, SOL_SOCKET, SO_ERROR, (void *) &err, &len)
+	    == -1)
+	{
+	    err = ngx_errno;
+	}
+
+	if (err) {
+	    (void) ngx_connection_error(c, err, "connect() failed");
+	    return NGX_ERROR;
+	}
+    }
+
+    return NGX_OK;
 }
 
 

commit 1877c7abbb25f36a310fb3415f1b853ed26525f9

connection error should be logged with "connecting to upstream" action, the bug was introduced in r1154
--HG--
extra : convert_revision : svn%3Ac3fe7df1-7212-e011-8a91-001109144009/trunk%401470

src/http/ngx_http_upstream.c
@@ -1290,6 +1290,7 @@ ngx_http_upstream_test_connect(ngx_connection_t *c)
 
     if (ngx_event_flags & NGX_USE_KQUEUE_EVENT)  {
         if (c->write->pending_eof) {
+            c->log->action = "connecting to upstream";
             (void) ngx_connection_error(c, c->write->kq_errno,
                                     "kevent() reported that connect() failed");
             return NGX_ERROR;
@@ -1313,6 +1314,7 @@ ngx_http_upstream_test_connect(ngx_connection_t *c)
         }
 
         if (err) {
+            c->log->action = "connecting to upstream";
             (void) ngx_connection_error(c, err, "connect() failed");
             return NGX_ERROR;
         }