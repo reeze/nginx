reeze/nginx

commit 4b35d9f86a019cde1a2ad22203d3d8fac5e6d343

when the FastCGI header was split in records, nginx passed garbage in the header to a client
git-svn-id: svn://svn.nginx.org/nginx/trunk@1273 c3fe7df1-7212-e011-8a91-001109144009

src/http/modules/ngx_http_fastcgi_module.c
@@ -1144,7 +1144,7 @@ ngx_http_fastcgi_process_header(ngx_http_request_t *r)
             return NGX_OK;
         }
 
-        if (u->buffer.pos == u->buffer.last) {
+        if (rc == NGX_OK && u->buffer.pos == u->buffer.last) {
             return NGX_AGAIN;
         }
 

commit 059c00fe754b124bd2c490ffb1888c42ad337331

fix error introduced in r1273 and r1275: nginx did not process FastCGI response if header was at end of FastCGI record
--HG--
extra : convert_revision : svn%3Ac3fe7df1-7212-e011-8a91-001109144009/trunk%401999

src/http/modules/ngx_http_fastcgi_module.c
@@ -1232,7 +1232,7 @@ ngx_http_fastcgi_process_header(ngx_http_request_t *r)
         }
 
         if (rc == NGX_OK) {
-            return NGX_AGAIN;
+            continue;
         }
 
         /* rc == NGX_AGAIN */