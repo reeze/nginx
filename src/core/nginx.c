reeze/nginx

commit 9ea043d4d3d9a86e7c81a0e8bf38771267073f9f

move ngx_strerror_init() at the very start
git-svn-id: svn://svn.nginx.org/nginx/trunk@3799 c3fe7df1-7212-e011-8a91-001109144009

src/core/nginx.c
@@ -203,6 +203,10 @@ main(int argc, char *const *argv)
     ngx_cycle_t      *cycle, init_cycle;
     ngx_core_conf_t  *ccf;
 
+    if (ngx_strerror_init() != NGX_OK) {
+        return 1;
+    }
+
     if (ngx_get_options(argc, argv) != NGX_OK) {
         return 1;
     }
@@ -269,10 +273,6 @@ main(int argc, char *const *argv)
 #endif
 
     ngx_pid = ngx_getpid();
-
-    if (ngx_strerror_init() != NGX_OK) {
-        return 1;
-    }
 
     log = ngx_log_init(ngx_prefix);
     if (log == NULL) {

commit 1c43b19ed0b65d474f05ea55a106381dbf214cd1

FreeBSD's MALLOC_OPTIONS must be set before any malloc() call. The bug has been introduced in r3799.
--HG--
extra : convert_revision : svn%3Ac3fe7df1-7212-e011-8a91-001109144009/trunk%404223

src/core/nginx.c
@@ -203,6 +203,10 @@ main(int argc, char *const *argv)
     ngx_cycle_t      *cycle, init_cycle;
     ngx_core_conf_t  *ccf;
 
+#if (NGX_FREEBSD)
+    ngx_debug_init();
+#endif
+
     if (ngx_strerror_init() != NGX_OK) {
         return 1;
     }
@@ -259,10 +263,6 @@ main(int argc, char *const *argv)
             return 0;
         }
     }
-
-#if (NGX_FREEBSD)
-    ngx_debug_init();
-#endif
 
     /* TODO */ ngx_max_sockets = -1;
 